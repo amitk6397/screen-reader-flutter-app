3e9dc682a285775182ecff1dd714c40795c4f563
kritidigitalsolutions <info@kritidigital.com>
2026-05-20 11:08:45 +0530

    create custom res file

---
+++ lib/data/network/network_api_service.dart
@@ -0,0 +1,171 @@
+import 'package:dio/dio.dart';
+import 'package:flutter/foundation.dart';
+
+import 'baes_api_service.dart';
+
+class NetworkApiService extends BaseApiService {
+  late Dio _dio;
+
+  NetworkApiService() {
+    _dio = Dio(
+      BaseOptions(
+        connectTimeout: const Duration(seconds: 20),
+        receiveTimeout: const Duration(seconds: 20),
+        headers: {"Content-Type": "application/json"},
+      ),
+    );
+
+    /// Interceptor for logging
+    _dio.interceptors.add(
+      InterceptorsWrapper(
+        onRequest: (options, handler) {
+          debugPrint("➡️ REQUEST [${options.method}] => ${options.uri}");
+          debugPrint("Headers: ${options.headers}");
+          debugPrint("Data: ${options.data}");
+          return handler.next(options);
+        },
+        onResponse: (response, handler) {
+          debugPrint("✅ RESPONSE [${response.statusCode}] => ${response.data}");
+          return handler.next(response);
+        },
+        onError: (DioException e, handler) {
+          debugPrint("❌ ERROR [${e.response?.statusCode}] => ${e.message}");
+          return handler.next(e);
+        },
+      ),
+    );
+  }
+
+  /// 🔑 Set Authorization Token
+  void setToken(String token) {
+    _dio.options.headers["Authorization"] = "Bearer $token";
+    debugPrint("🔐 Token Set: Bearer $token");
+  }
+
+  /// ❌ Remove Token (Logout)
+  void clearToken() {
+    _dio.options.headers.remove("Authorization");
+    debugPrint("🔓 Token Cleared");
+  }
+
+  @override
+  Future<dynamic> getApi(String url) async {
+    try {
+      debugPrint("GET API CALL => $url");
+      final response = await _dio.get(url);
+      return response.data;
+    } on DioException catch (e) {
+      debugPrint("GET API ERROR => ${e.message}");
+      if (e.response != null) {
+        // 👇 IMPORTANT: return backend response even on 400
+        return e.response!.data;
+      }
+      throw Exception("No Internet");
+    }
+  }
+
+  @override
+  Future<dynamic> postApi(String url, dynamic data) async {
+    try {
+      debugPrint("POST API CALL => $url");
+      debugPrint("POST DATA => $data");
+
+      final response = await _dio.post(url, data: data);
+      return response.data;
+    } on DioException catch (e) {
+      debugPrint("POST API ERROR => ${e.message}");
+      if (e.response != null) {
+        // 👇 IMPORTANT: return backend response even on 400
+        return e.response!.data;
+      }
+      throw Exception("No Internet");
+    }
+  }
+
+  @override
+  Future<dynamic> pacthApi(String url, dynamic data) async {
+    try {
+      debugPrint("POST API CALL => $url");
+      debugPrint("POST DATA => $data");
+
+      final response = await _dio.patch(url, data: data);
+      return response.data;
+    } on DioException catch (e) {
+      debugPrint("POST API ERROR => ${e.message}");
+      if (e.response != null) {
+        // 👇 IMPORTANT: return backend response even on 400
+        return e.response!.data;
+      }
+      throw Exception("No Internet");
+    }
+  }
+
+  @override
+  Future<dynamic> putApi(String url, dynamic data) async {
+    try {
+      debugPrint("PUT API CALL => $url");
+      debugPrint("PUT DATA => $data");
+
+      final response = await _dio.put(url, data: data);
+      return response.data;
+    } on DioException catch (e) {
+      debugPrint("PUT API ERROR => ${e.message}");
+      if (e.response != null) {
+        // 👇 IMPORTANT: return backend response even on 400
+        return e.response!.data;
+      }
+      throw Exception("No Internet");
+    }
+  }
+
+  @override
+  Future<dynamic> deleteApi(String url, dynamic data) async {
+    try {
+      debugPrint("DELETE API CALL => $url");
+
+      final response = await _dio.delete(url, data: data);
+      return response.data;
+    } on DioException catch (e) {
+      debugPrint("DELETE API ERROR => ${e.message}");
+      if (e.response != null) {
+        // 👇 IMPORTANT: return backend response even on 400
+        return e.response!.data;
+      }
+      throw Exception("No Internet");
+    }
+  }
+
+  // AppException _handleDioError(DioException error) {
+  //   debugPrint("HANDLE ERROR => ${error.response?.data}");
+
+  //   switch (error.type) {
+  //     case DioExceptionType.connectionTimeout:
+  //     case DioExceptionType.sendTimeout:
+  //     case DioExceptionType.receiveTimeout:
+  //       return FetchDataException("Connection timeout");
+
+  //     case DioExceptionType.badResponse:
+  //       final statusCode = error.response?.statusCode ?? 0;
+  //       final message = error.response?.data.toString() ?? "Unknown error";
+
+  //       if (statusCode == 400) {
+  //         return BadRequestException(message);
+  //       } else if (statusCode == 401 || statusCode == 403) {
+  //         return UnauthorizedException(message);
+  //       } else if (statusCode == 500) {
+  //         return FetchDataException("Server Error");
+  //       } else {
+  //         return FetchDataException(
+  //           "Error occurred with status code : $statusCode",
+  //         );
+  //       }
+
+  //     case DioExceptionType.cancel:
+  //       return FetchDataException("Request cancelled");
+
+  //     case DioExceptionType.unknown:
+  //     default:
+  //       return FetchDataException("No Internet Connection");
+  //   }
+  // }
+}
--- lib/res/app_colors.dart
+++ lib/res/app_colors.dart
@@ -36,4 +36,62 @@ class AppColors {
   static const Color coverOrange = Color(0xFF993C1D);
   static const Color coverTeal = Color(0xFF0F6E56);
   static const Color coverPink = Color(0xFF993556);
+
+  // Primary Colors
+  static const Color primary = Color(0xFF06192B);
+  static const Color lightBlue = Color(0xFFE2F5FF);
+
+  // Background Colors
+  static const Color background = Color(0xFFF7F5F1);
+
+  // Text Colors
+
+  static const Color hintText = Color(0xFFBDBDBD);
+
+  // Button Colors
+  static const Color button = primary;
+  static const Color blue = Color(0xFF059AE4);
+  static const Color buttonText = Color(0xFF000000);
+
+  // Border & Divider
+  static const Color divider = Color(0xFFF2F2F2);
+
+  // Status Colors
+
+  static const Color error = Color(0xFFEB5757);
+
+
+  // Other
+  static const Color transparent = Colors.transparent;
+
+  static const grey = Colors.grey;
+
+  static const grey50 = Color(0xFFF5F5F5); // same as Colors.grey[50]
+  static const grey100 = Color(0xFFF0F0F0);
+  static const grey200 = Color(0xFFEEEEEE);
+  static const grey300 = Color(0xFFE0E0E0);
+  static const grey400 = Color(0xFFBDBDBD);
+  static const grey500 = Colors.grey;
+  static const grey600 = Color(0xFF757575);
+  static const grey700 = Color(0xFF616161);
+  static const grey800 = Color(0xFF424242);
+  static const grey900 = Color(0xFF212121);
+
+  // Black variants
+  static const black = Colors.black;
+  static const black87 = Colors.black87;
+  static const black54 = Colors.black54;
+  static const black45 = Colors.black45;
+  static const black12 = Colors.black12;
+
+  // White variants
+  static const white = Colors.white;
+  static const white70 = Colors.white70;
+  static const white54 = Colors.white54;
+  static const white38 = Colors.white38;
+  static const white30 = Colors.white30;
+
+  static const yellow = Color(0xFFDBBC00);
+
+  static const orange = Colors.deepOrange;
 }
